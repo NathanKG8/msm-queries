7fde60c491e5c25368f50a593c7f66d3c1d9540d
NathanKG8 <164825742+NathanKG8@users.noreply.github.com>
2024-05-01 03:43:43 +0000

    Progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/youngest", { :controller => "misc", :action => "homepage" })
+
+  get("/directors/eldest", { :controller => "misc", :action => "homepage" })
+
+  get("/movies", { :controller => "misc", :action => "homepage" })
+
+  get("/actors", { :controller => "misc", :action => "homepage" })
 end
